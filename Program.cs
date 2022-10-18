06fbef1feb2f8edcde9938c00c76f858e46596b8
GriffinSwain <truckfan123@gmail.com>
2022-10-17 21:34:40 -0700

    Defeating Gremlins

--- Program.cs
+++ Program.cs
@@ -3,17 +3,23 @@
 //October 17, 2022
 //Peer reviewed by:
 
+
+//Initializes the various variables in the main method of the program. 
+//This is where the parts of the face that change are stored.
 string name = "";
 int loop = 1;
 int talk = 0;
 int eyes = 0;
 int jaw = 0;
+int second =  0;
 string end = "o";
-string mouthSmile = "   :  \\_____/  :   ";
-string mouthOpen =  "   :  _______  :";
+string mouthClosed = "   :  -------  :  ";
+string mouthSmile =  "   :  \\_____/ :   ";
+string mouthOpen =   "   :  ______   :";
 string eyesOpen =   ":(:  (O)   (O)  :):";
 string eyesClosed = ":(:  (_)   (_)  :):";
-string eyesWink =   ":(:  <0>   <0>  :):";
+string eyesWink =   ":(:  <0>   <_>~#:):";
+string specialEyes =":(:  <0>   <0>  :):";
 string greetings = "Hello";
 string sentence = "I hope you have a good day today!";
 bool choice = false;
@@ -23,35 +29,43 @@ while (loop == 1)
 {
 while (name.Length <= 0)
 { 
-Console.WriteLine("Hello there! Please give me your name so that I can properly greet you!");
+Console.WriteLine("Hello there!  ");
+Task.Delay(750).Wait();
+Console.WriteLine("Please give me your name so that I can properly greet you!");
 name = Console.ReadLine();
 }
 while (talk <= 5)
 {
 Console.Clear();
 jaw = 1;
-Face(mouthOpen, name, greetings, sentence, eyesOpen, jaw);
-Thread.Sleep(1000);
+Face(mouthOpen, name, greetings, sentence, eyesOpen, jaw, second);
+Thread.Sleep(600);
 Console.Clear();
 if (eyes % 3 == 1)
 {
     jaw = 0;
-    Face(mouthSmile, name, greetings, sentence, eyesClosed, jaw);
+    Face(mouthClosed, name, greetings, sentence, eyesClosed, jaw, second);
 }
 else
 {
     jaw = 0;
-    Face(mouthSmile, name, greetings, sentence, eyesOpen, jaw);
+    Face(mouthClosed, name, greetings, sentence, eyesOpen, jaw, second);
 }
-Thread.Sleep(1000);
+Thread.Sleep(400);
+if (talk >= 2)
+{
+second++;
+}
 talk++;
 eyes++;
 }
 
+second--;
 choice = false;
 
 while (choice == false)
 {
+Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine("Would you like to me to greet you again? Type \"yes\" to play again or \"no\" to end the program");
 end = Console.ReadLine();
@@ -61,25 +75,62 @@ if (end == "yes" || end == "no"){
     talk = 0;
     eyes = 0;
     jaw = 0;
+    second = 0;
 }
 }
 
 if (end == "no")
 {
+
+    while (talk <= 2)
+{
+Console.Clear();
+sentence = "Thanks for letting me greet you! It's the reason I was created!";
+greetings = "Goodbye";
+jaw = 1;
+Face(mouthOpen, name, greetings, sentence, eyesOpen, jaw, second);
+Thread.Sleep(600);
+Console.Clear();
+if (eyes % 3 == 1)
+{
+    jaw = 0;
+    Face(mouthClosed, name, greetings, sentence, eyesClosed, jaw, second);
+}
+else
+{
+    jaw = 0;
+    Face(mouthClosed, name, greetings, sentence, eyesOpen, jaw, second);
+}
+if (jaw >= 1){
+    second++;
+}
+Thread.Sleep(400);
+    talk++;
     loop++;
+    
+}
 }
 name = "";
 Console.Clear();
 }
 
-name = "for now";
-greetings = "Goodbye";
+name = "=~for now~=";
+greetings = "Goodbye...";
 sentence = "Thanks for letting me greet you! It's the reason I was created!";
-Face(mouthSmile, name, greetings, sentence, eyesWink, jaw);
+Face(mouthSmile, name, greetings, sentence, specialEyes, jaw, second);
+Thread.Sleep(1750);
+Console.Clear();
 
-static void Face(string mouth, string name, string greetings, string sentence, string eyes, int jaw)
+Face(mouthSmile, name, greetings, sentence, eyesWink, jaw, second);
+Thread.Sleep(400);
+Console.Clear();
+second++;
+Face(mouthSmile, name, greetings, sentence, specialEyes, jaw, second);
+Console.WriteLine();
+Console.WriteLine();
+
+static void Face(string mouth, string name, string greetings, string sentence, string eyes, int jaw, int second)
 {
-    int open = 1;
 Console.WriteLine("      .......");
 Console.WriteLine("  .:::::::::::::.  ");
 Console.WriteLine(" .::'  '''''  '::. ");
@@ -96,7 +147,13 @@ if (jaw == 1)
 {
 Console.WriteLine("   :  \\_____/  :");
 }
-Console.WriteLine($"   '.  ~:::~  .' {sentence}");
+Console.WriteLine($"   '.  ~:::~  .' ");
 Console.WriteLine("     '.  '  .'     ");
-Console.WriteLine("       '''''");
+Console.WriteLine("       '''''");;
+if (second > 0)
+{
+Console.SetCursorPosition(0, Console.CursorTop - 3);
+Console.WriteLine($"   '.  ~:::~  .' {sentence}");
+}
+Console.WriteLine();
 }
